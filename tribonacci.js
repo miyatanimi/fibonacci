cadbbabf4af3627c93a4adc4104e203e2b6de6e4
miyata <k.paddy123@gmail.com>
2020-04-20 23:49:39 +0900

    トリボナッチ数列用の関数作成

---
+++ tribonacci.js
@@ -0,0 +1,19 @@
+'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,0);
+memo.set(2,1);
+
+function trib(n) {
+    if(memo.has(n)){
+        return memo.get(n);
+    }
+    const value = memo.get(n-1) + memo.get(n-2) + memo.get(n-3);
+    memo.set(n,value);
+    return value;
+}
+
+const length = 40;
+for(let i=0; i<=length; i++){
+    console.log(trib(i));
+}
